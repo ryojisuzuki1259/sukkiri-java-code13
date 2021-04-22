b3ebfead36c59a87d2cc5dbd96b300f6e9785bee
suzukiryoji <suzukiryoji@localhost>
2021-04-23 00:23:29 +0900

    Write codes

--- src/code13/Hero.java
+++ src/code13/Hero.java
@@ -9,4 +9,9 @@ public class Hero {
 		this.hp = 100;
 		System.out.println(this.name + "は、眠って回復した！");
 	}
+
+	private void die() {
+		System.out.println(this.name + "は死んでしまった！");
+		System.out.println("GAME OVERです。");
+	}
 }
